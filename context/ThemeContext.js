596976504d193f7b1fb3327f69c8eca0dcf2a1e2
samuel-yeebiyo <samoyemaneyy@gmail.com>
2023-04-01 16:33:50 +0000

    Changed background to gradients, added proper favicon, and added glassmorphisim to navigation bar

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -11,32 +11,29 @@ export const  ContextWrapper = ({children}) => {
         if ( localTheme == 'light'){
             setLight()
         } else setDark()
-
-        console.log('Fetching')
-
     },[])
 
     useEffect(()=>{
         if(theme != '') localStorage.setItem('theme', theme)
-        console.log("Setting")
-
     },[theme])
 
     const setDark = () =>{
         let root = document.querySelector(':root');
-        root.style.setProperty('--background', 'rgb(20,20,20)')
+        // root.style.setProperty('--background', 'rgb(20,20,20)')
+        root.style.setProperty('--background', 'linear-gradient(147deg, #000000 0%, #2c3e50 74%)')
         root.style.setProperty('--text', 'white')
         root.style.setProperty('--secondary', 'rgb(43, 43, 43)')
         root.style.setProperty('--button-background', 'white')
         root.style.setProperty('--button-color', 'black')
         root.style.setProperty('--fill', 'white')
-
+        
         setTheme('dark')
     }
-
+    
     const setLight = () => {
         let root = document.querySelector(':root');
-        root.style.setProperty('--background', 'white')
+        // root.style.setProperty('--background', 'white')
+        root.style.setProperty('--background', 'linear-gradient(315deg, #b8c6db 0%, #f5f7fa 74%)')
         root.style.setProperty('--text', 'black')
         root.style.setProperty('--secondary', 'rgb(200,200,200)')
         root.style.setProperty('--button-background', 'black')
